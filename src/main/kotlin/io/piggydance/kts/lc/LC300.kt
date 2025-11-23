d1770ef7cb93e795b057be999a9bdf6208a32a30
changjianfei <changjianfei@bytedance.com>
2025-11-23 23:33:46 +0800

    LC300

--- src/main/kotlin/io/piggydance/kts/lc/LC300.kt
+++ src/main/kotlin/io/piggydance/kts/lc/LC300.kt
@@ -1,15 +1,23 @@
 package io.piggydance.kts.lc
 
+import kotlin.math.max
+
 class LC300 {
     fun lengthOfLIS(nums: IntArray): Int {
         // nums[i] > nums[j]
         // dp[i] = max(dp[i], dp[j] + 1)
-        val dp = IntArray(nums.size)
+        val dp = IntArray(nums.size + 1)
         dp[0] = 1
-//        for (i in 1..< nums.size) {
-//            for (j in 0..< i) {
-//                if (nums[])
-//            }
-//        }
+        var maxans = 1
+        for (i in 1..< nums.size) {
+            dp[i] = 1
+            for (j in 0..< i) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1)
+                }
+            }
+            maxans = max(maxans, dp[i])
+        }
+        return maxans
     }
 }
--- src/test/kotlin/io/piggydance/kts/lc/LC300Test.kt
+++ src/test/kotlin/io/piggydance/kts/lc/LC300Test.kt
@@ -10,6 +10,7 @@ class LC300Test {
         assertEquals(4, s.lengthOfLIS(intArrayOf(10,9,2,5,3,7,101,18)))
         assertEquals(4, s.lengthOfLIS(intArrayOf(0,1,0,3,2,3)))
         assertEquals(1, s.lengthOfLIS(intArrayOf(7,7,7,7,7,7,7)))
+        assertEquals(3, s.lengthOfLIS(intArrayOf(4,10,4,3,8,9)))
     }
 
 }
