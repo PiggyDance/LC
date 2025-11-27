5cd1123ad9ec18825dd0a846934a9a2f2468118e
changjianfei <changjianfei@bytedance.com>
2025-11-27 20:17:42 +0800

    LC41 addition

--- src/main/kotlin/io/piggydance/kts/lc/LC41.kt
+++ src/main/kotlin/io/piggydance/kts/lc/LC41.kt
@@ -16,4 +16,24 @@ class LC41 {
         }
         return nums.size + 1
     }
+
+    fun firstMissingPositive2(numbers: IntArray): Int {
+        val nums = IntArray(numbers.size + 1)
+        numbers.copyInto(nums, 1)
+        // 2, 1, 3, 5
+        for (i in 1..< nums.size) {
+            while (nums[i] >= 1 && nums[i] <= numbers.size && nums[i] != i && nums[i] != nums[nums[i]]) {
+                val j = nums[i]
+                val tmp = nums[j]
+                nums[j] = nums[i]
+                nums[i] = tmp
+            }
+        }
+        for (i in 1..< nums.size) {
+            if (nums[i] != i) {
+                return i
+            }
+        }
+        return nums.size
+    }
 }
--- src/test/kotlin/io/piggydance/kts/lc/LC41Test.kt
+++ src/test/kotlin/io/piggydance/kts/lc/LC41Test.kt
@@ -10,5 +10,17 @@ class LC41Test {
         val s = LC41()
         assertEquals(3, s.firstMissingPositive(intArrayOf(1, 2, 0)))
         assertEquals(2, s.firstMissingPositive(intArrayOf(3, 4, -1, 1)))
+        assertEquals(1, s.firstMissingPositive(intArrayOf(2, 2)))
+        assertEquals(2, s.firstMissingPositive(intArrayOf(1, 1)))
+    }
+
+    @Test
+    fun `test basic case 2`() {
+        val s = LC41()
+        assertEquals(3, s.firstMissingPositive2(intArrayOf(1, 2, 0)))
+        assertEquals(2, s.firstMissingPositive2(intArrayOf(3, 4, -1, 1)))
+        assertEquals(1, s.firstMissingPositive2(intArrayOf(2, 2)))
+        assertEquals(2, s.firstMissingPositive2(intArrayOf(1, 1)))
+        assertEquals(1, s.firstMissingPositive2(intArrayOf(2)))
     }
 }
